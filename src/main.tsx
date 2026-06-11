f46cadf9657109b38c8a71ef7204fcce95c2a066
luiscr918 <lc9107040@gmail.com>
2026-06-11 12:45:28 -0500

    intento de integracion de analitycs 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import TagManager from "react-gtm-module";
-
-const gtmId = import.meta.env.VITE_GTM_ID as string | undefined;
-
-if (gtmId) {
-  TagManager.initialize({ gtmId });
-
-  // Insert GTM noscript iframe for users with JS disabled
-  if (typeof document !== "undefined") {
-    const noscript = document.createElement("noscript");
-    noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
-    document.body.insertBefore(noscript, document.body.firstChild);
-  }
-} else {
-  console.warn(
-    "VITE_GTM_ID no está definido en el entorno. GTM no inicializado.",
-  );
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
